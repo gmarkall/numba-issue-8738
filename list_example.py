ab1e6307181c8466f167d77b4137a182601b3971
Graham Markall <gmarkall@nvidia.com>
2023-03-08 11:48:47 +0000

    simpifly

--- list_example.py
+++ list_example.py
@@ -4,8 +4,10 @@ from numba.typed import List
 mylist = List()
 for i in range(10):
     l = List()
-    for i in range(10):
-        l.append(i)
+    l.append(0)
+    print("toot")
+    #for i in range(10):
+    #    l.append(i)
     mylist.append(l)
 
 print(mylist)
