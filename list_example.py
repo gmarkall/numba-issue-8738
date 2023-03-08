8388fbc263bad2e776b2fe40e5602b01489f18e5
Graham Markall <gmarkall@nvidia.com>
2023-03-08 11:49:47 +0000

    Simplify

--- list_example.py
+++ list_example.py
@@ -2,9 +2,8 @@ from numba import njit
 from numba.typed import List
 
 mylist = List()
-for i in range(10):
-    l = List()
-    l.append(0)
-    mylist.append(l)
+l = List()
+l.append(0)
+mylist.append(l)
 
 print(mylist)
