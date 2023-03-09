e6d7e9f80a840ea0600ee7e3344bb0f655aa0bef
Graham Markall <gmarkall@nvidia.com>
2023-03-09 14:53:01 +0000

    Add option to turn debug on

--- list_example.py
+++ list_example.py
@@ -1,6 +1,10 @@
+from llvmlite.binding.options import set_debug, set_option
+
 from numba import njit, int64
 from numba.types import ListType
 from numba.typed import List
+
+#set_debug(True)
 
 mylist = List()
 l = List(lsttype=ListType(int64))
