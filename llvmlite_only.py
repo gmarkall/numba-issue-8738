645909b242dd89ff09f664c03dbedc421f641331
Graham Markall <gmarkall@nvidia.com>
2023-03-10 11:12:01 +0000

    Show correct function disass, show pid

--- llvmlite_only.py
+++ llvmlite_only.py
@@ -2,6 +2,7 @@ import llvmlite.binding as llvm
 import ctypes
 import tempfile
 import subprocess
+import os
 
 # llvm.options.set_debug(True)
 
@@ -23,9 +24,12 @@ engine.finalize_object()
 fname = ('_ZN5numba5typed10listobject13impl_new_list12_3clocals_'
          '3e3impB2v4B42c8tJTIcFHzwl2ILiXkcBV0KBSmNGHkyiCKJEEwA_'
          '3dE32typeref_5bListType_5bint64_5d_5dx')
-addr = engine.get_function_address(fname)
+fnam2 = ('_ZN5numba5typed9typedlist10_make_listB2v3B38c8tJTIcFHzw'
+         'l2ILiXkcBV0KBSgP9CGZpAgA_3dE32typeref_5bListType_5bint64_5d_5dx')
+addr = engine.get_function_address(fnam2)
 
 print(f"Function is at 0x{addr:x}")
+print(f"PID is {os.getpid()}")
 
 with tempfile.NamedTemporaryFile() as f:
     data = (ctypes.c_char*256).from_address(addr)
@@ -36,3 +40,4 @@ with tempfile.NamedTemporaryFile() as f:
     cp = subprocess.run(objdump_cmd.split(' '), capture_output=True)
     print(cp.stdout.decode())
 
+input()
